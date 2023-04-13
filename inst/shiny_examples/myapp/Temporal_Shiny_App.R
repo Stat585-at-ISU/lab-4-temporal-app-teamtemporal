fe9503b6f3629ca7d37616a6be2406bd0d6053d2
luobo119 <bailuobo119@hotmail.com>
2023-04-13 15:02:44 -0500

    framework

--- inst/shiny_examples/myapp/Temporal_Shiny_App.R
+++ inst/shiny_examples/myapp/Temporal_Shiny_App.R
@@ -1,8 +1,28 @@
 #Our shiny app!
 ui <- fluidPage(
-  dateRangeInput("daterange", "Date range:",
-                 start = "2001-01-01",
-                 end   = "2010-12-31")
+  # App title ----
+  titlePanel("AmesPD"),
+
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      dateRangeInput("daterange", "Date range:",
+                     start = "2001-01-01",
+                     end   = "2010-12-31")
+
+    ),
+
+    # Main panel for displaying outputs ----
+    mainPanel(
+
+      tabsetPanel(type = "tabs",
+                  tabPanel("A"),
+                  tabPanel("B")
+      )
+    )
+  )
 
 )
 server <- function(input, output) {}
