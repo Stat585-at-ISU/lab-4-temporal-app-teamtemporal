515863e0582c9be88f44d196593ab6a4d9935172
Sabrena Rutledge <sabrenar@iastate.edu>
2023-04-13 13:35:15 -0500

    adding ui format

--- inst/shiny_examples/myapp/Temporal_Shiny_App.R
+++ inst/shiny_examples/myapp/Temporal_Shiny_App.R
@@ -1,1 +1,26 @@
 #Our shiny app!
+library(shiny)
+
+#Read in data and get data range
+
+
+#ui function
+ui <- fluidPage(titlePanel("AmesPD Press-logs: A Temporal Analysis",
+                sidebarLayout(sidebarPanel( # Inputs(Widgets) go here:
+                              ))
+      ,mainPanel(tabsetPanel(
+        plotOutput(outputId = "distPlot")
+      ), tabsetPanel(
+        plotOutput(outputId = "distPlot")
+      )
+      )
+))
+
+
+
+
+
+
+#server function
+
+
