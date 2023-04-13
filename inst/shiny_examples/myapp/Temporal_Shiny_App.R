0fb6a4af3555079166c915ffcc17de350fcff521
luobo119 <bailuobo119@hotmail.com>
2023-04-13 14:58:56 -0500

    daterange input

--- inst/shiny_examples/myapp/Temporal_Shiny_App.R
+++ inst/shiny_examples/myapp/Temporal_Shiny_App.R
@@ -1,1 +1,9 @@
 #Our shiny app!
+ui <- fluidPage(
+  dateRangeInput("daterange", "Date range:",
+                 start = "2001-01-01",
+                 end   = "2010-12-31")
+
+)
+server <- function(input, output) {}
+shinyApp(ui, server)
