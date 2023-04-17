1a3c5b8dd9a6786e4953de69e2d0779136280041
kundan kumar <kkundan7kumar@gmail.com>
2023-04-17 09:45:26 -0500

    map!

--- inst/shiny_examples/myapp/tempR.R
+++ inst/shiny_examples/myapp/tempR.R
@@ -3,6 +3,8 @@ library(tidyverse)
 library(plotly)
 library(devtools)
 library(leaflet)
+library(shiny)
+
 
 #Read in data and get data range
 load_all('./')
@@ -10,44 +12,27 @@ load_all('./')
 data("presslog_isu")
 data("presslog_ames")
 
-presslog_ames2 <- separate(presslog_ames, "Call Received Date/Time", c("Date.Reported", "Time.Reported"), sep=" ", fill="right")
-presslog_ames2$Date.Reported <- ymd(presslog_ames2$Date.Reported)
-ames_date_range <- c(min(presslog_ames2$Date.Reported), max(presslog_ames2$Date.Reported))
+data("location")
+locations <- read.csv("inst/location.csv")
+#library(readr)
 
-presslog_isu2 <- separate(presslog_isu, "Date.Time.Reported", c("Date.Reported", "Time.Reported"), sep=" ", fill="right")
-presslog_isu2$Date.Reported <- ymd(presslog_isu2$Date.Reported)
-isu_date_range <- c(min(presslog_isu2$Date.Reported), max(presslog_isu2$Date.Reported))
+#locations <- read_csv("/Users/kundankumar/Documents/STAT_585/lab-4-temporal-app-teamtemporal/inst/locations.csv")
+#View(locations)
+
 
 # Define UI
 ui <- fluidPage(
-  titlePanel("AmesPD Press-logs: A Temporal Analysis"),
-  sidebarLayout(
-    sidebarPanel(
-      dateRangeInput("dateRange", "Date range:", start=ames_date_range[1], end=ames_date_range[2])),
-
-      #dateRangeInput("dateRange", "Date range:", start=asu_date_range[1], end=asu_date_range[2])),
-    mainPanel(
-      leafletOutput(outputId = "map")
-    )
-  )
+  leafletOutput("map")
 )
 
-# Define server
+
 server <- function(input, output) {
-
-  # Filter data based on date range input
-  filtered_data <- reactive({
-    presslog_isu2 %>%
-      filter(Date.Reported >= input$dateRange[1] & Date.Reported <= input$dateRange[2])
-  })
-
-  # Create map of incidents
   output$map <- renderLeaflet({
-    leaflet(data = filtered_data()) %>%
+    leaflet(locations) %>%
       addTiles() %>%
-      addMarkers(lng = ~Long, lat = ~Lat, popup = ~Incident.Description)
+      addMarkers(lng = ~longitude, lat = ~latitude, popup = ~General.Location)
   })
 }
 
-# Run app
+
 shinyApp(ui, server)
